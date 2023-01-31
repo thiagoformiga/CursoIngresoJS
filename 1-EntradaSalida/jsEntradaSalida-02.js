0a49074edb3c65569b4c08572a22e4f16d669ab2
thiagoformiga <formigathiago@gmail.com>
2023-01-31 17:53:51 -0300

    nuevo cambios

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -5,7 +5,9 @@ y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
 	let nombrePersona; //declaracion de variable 
+	let apellidopersona
 	nombrepersona = prompt ("¿cúal es tu nombre?")
-	alert(nombrepersona);
+	apellidopersona = prompt ("¿cúal es tu apelldo?")
+	alert("su nombre es "+nombrepersona + " su apellido es "+apellidopersona);
 }
 
