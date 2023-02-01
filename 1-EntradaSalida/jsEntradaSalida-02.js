958b31cc76d8fe69641d1d958ce5bf2fc7bde218
thiagoformiga <formigathiago@gmail.com>
2023-01-31 21:46:06 -0300

    nuevo ejercicio

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -8,6 +8,6 @@ function mostrar()
 	let apellidopersona
 	nombrepersona = prompt ("¿cúal es tu nombre?")
 	apellidopersona = prompt ("¿cúal es tu apelldo?")
-	alert("su nombre es "+nombrepersona + " su apellido es "+apellidopersona);
+	alert("su nombre es "+nombrepersona + "su apellido es "+apellidopersona);
 }
 
