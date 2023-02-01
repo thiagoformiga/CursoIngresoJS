e1427a90b1c8777183d2da25403b7fa22ecfe5f6
thiagoformiga <formigathiago@gmail.com>
2023-02-01 18:21:54 -0300

    new

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -4,11 +4,24 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	let nombreIngresado;
+	let Producto;
+	let precioDelProducto;
+	let porcentaje;
+	let precioFinal;
 	let mensaje;
 	
-	nombreingresado = document.getElementById("txtIdNombre").value;
-	mensaje= ("Su nombre es "+ nombreingresado);
+	Producto= prompt("introducir nombre del producto");
+
+	precioDelProducto = document .getElementById("txtIdNombre").value;
+	
+	precioDelProducto = parseInt(precioDelProducto);
+
+	porcentaje = (precioDelProducto * 30 / 100);
+
+	precioFinal = (precioDelProducto + porcentaje);
+
+	mensaje = "el nombre del producto es " + Producto  + " y su precio es de " + precioFinal;
+	
 	
 	alert(mensaje);
 
